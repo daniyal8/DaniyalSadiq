689007e9a249ba95f109875f72250c56809fc005
daniyal8 <daniyalsadiq4@gmail.com>
2020-07-24 20:28:32 +0500

    feat: portfolio with html and css

---
+++ Js/main.js
@@ -0,0 +1,79 @@
+const filtercontainer = document.querySelector(".portfolio-filter");
+        filterbtns = filtercontainer.children,
+        totalfilterbtn = filterbtns.length,
+        portfolioItems=document.querySelectorAll(".portfolio-item"),
+        totalportfolioItem = portfolioItems.length;
+        
+
+        for(let i=0; i<totalfilterbtn; i++){
+          filterbtns[i].addEventListener("click", function(){
+              filtercontainer.querySelector(".active").classList.remove("active")
+              this.classList.add("active")
+
+              const filterValue = this.getAttribute("data-filter");
+              for(let k=0; k<totalportfolioItem; k++){
+                  if(filterValue === portfolioItems[k].getAttribute("data-category")){
+                    portfolioItems[k].classList.remove("hide");
+                      portfolioItems[k].classList.add("show");
+                  }
+                  else{
+                    portfolioItems[k].classList.remove("show");
+                    portfolioItems[k].classList.add("hide");
+                  }
+                  if(filterValue === "all"){
+                    portfolioItems[k].classList.remove("hide");
+                    portfolioItems[k].classList.add("show");
+                  }
+              }
+          })
+        }
+
+     const lightbox = document.querySelector(".lightbox"),
+          lightboxImg = lightbox.querySelector(".lightbox-img"),
+          lightboxClose = lightbox.querySelector(".lightbox-close"),
+          lightboxText = lightbox.querySelector(".caption-text"),
+          lightboxCounter = lightbox.querySelector(".caption-counter");
+    let itemIndex = 0;
+    
+    for(let i=0; i<totalportfolioItem; i++){
+        portfolioItems[i].addEventListener("click", function(){
+           itemIndex = i;
+           changeItem();
+           toggleLightbox();
+        });
+    }
+    function nextItem(){
+        if(itemIndex === totalportfolioItem-1){
+            itemIndex=0;
+        }
+        else{
+            itemIndex++
+        }
+        changeItem();
+    }
+    function prevItem(){
+        if(itemIndex === 0){
+           itemIndex=totalportfolioItem-1
+        }
+        else{
+            itemIndex--
+        }
+        changeItem();
+    }
+     
+    function toggleLightbox(){
+        lightbox.classList.toggle("open")
+    }
+
+    function changeItem(){
+        imgSrc = portfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+        lightboxImg.src = imgSrc;
+        lightboxText.innerHTML = portfolioItems[itemIndex].querySelector("h4").innerHTML;
+        lightboxCounter.innerHTML= (itemIndex+1) + "of" + totalportfolioItem;
+    }
+
+    lightbox.addEventListener("click" ,function(event){
+        if(event.target === lightboxClose || event.target === lightbox){
+            toggleLightbox();
+        }
+    })
